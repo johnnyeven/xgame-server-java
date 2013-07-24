2dfd8b268d4801a42816284558a8a0abad1345bf
johnnyeven_work <johnnyeven@gmail.com>
2013-07-24 17:55:07 +0800

    1

--- GameServer/src/com/xgame/server/game/GameServer.java
+++ GameServer/src/com/xgame/server/game/GameServer.java
@@ -15,17 +15,23 @@ public class GameServer
 	
 	public void run()
 	{
-	    System.out.println( "MM   MM         MM   MM  MMMMM   MMMM   MMMMM");
-	    System.out.println( "MM   MM         MM   MM MMM MMM MM  MM MMM MMM");
-	    System.out.println( "MMM MMM         MMM  MM MMM MMM MM  MM MMM");
-	    System.out.println( "MM M MM         MMMM MM MMM     MM  MM  MMM");
-	    System.out.println( "MM M MM  MMMMM  MM MMMM MMM     MM  MM   MMM");
-	    System.out.println( "MM M MM M   MMM MM  MMM MMMMMMM MM  MM    MMM");
-	    System.out.println( "MM   MM     MMM MM   MM MM  MMM MM  MM     MMM");
-	    System.out.println( "MM   MM MMMMMMM MM   MM MMM MMM MM  MM MMM MMM");
-	    System.out.println( "MM   MM MM  MMM MM   MM  MMMMMM  MMMM   MMMMM");
-	    System.out.println( "        MM  MMM");
-	    System.out.println( "        MMMMMM\n\n");
+	    System.out.println( "                MMMM");
+	    System.out.println( " MMM    MMM    MMMMMMM                          MMMMMMMM");
+	    System.out.println( " MMM    MMM   MMMMMMMMM                         MMMMMMMM");
+	    System.out.println( "  MMM  MMM   MMMM   MMM                         MM");
+	    System.out.println( "  MMM  MMM   MMM    MMM    MMM    MM MMM  MMM   MM");
+	    System.out.println( "   MMMMMM    MMM     MM   MMMMMM  MMMMMMMMMMMMM MM");
+	    System.out.println( "   MMMMMM    MMM         MMMMMMM  MMMMMMMMMMMMM MM");
+	    System.out.println( "    MMMM     MM          MM   MM  MM   MMM  MMM MMMMMMMM");
+	    System.out.println( "    MMMM     MM   MMMMM       MM  MM   MMM   MM MMMMMMMM");
+	    System.out.println( "    MMMM     MM   MMMMM    MMMMM  MM   MMM   MM MM");
+	    System.out.println( "   MMMMMM    MMM     MM  MMMMMMM  MM   MMM   MM MM");
+	    System.out.println( "   MMMMMM    MMM     MM  MMMM MM  MM   MMM   MM MM");
+	    System.out.println( "  MMM  MMM   MMM     MM  MM   MM  MM   MMM   MM MM");
+	    System.out.println( " MMMM  MMMM  MMM    MMM  MM   MM  MM   MMM   MM MM");
+	    System.out.println( " MMM    MMM   MMMMMMMMM  MMMMMMMM MM   MMM   MM MMMMMMMM");
+	    System.out.println( "MMMM    MMMM   MMMMMMMM  MMMMMMMM MM   MMM   MM MMMMMMMM");
+	    System.out.println( "                 MMM MM    MM\n\n");
 	    
 	    World.getInstance().setInitialWorldSettings();
 	    
